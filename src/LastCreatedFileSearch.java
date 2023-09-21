e4d573fa84bcc0ab4003ec4f4ec73015c4d23ce5
Anastasiya <shirochina16@gmail.com>
2023-09-21 19:38:42 +0300

    refactor sort method

--- src/LastCreatedFileSearch.java
+++ src/LastCreatedFileSearch.java
@@ -1,64 +1,93 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.*;
+import java.util.function.ToLongFunction;
+import java.util.stream.Stream;
 
 public class LastCreatedFileSearch {
 
-            public static void main(String[] args) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Please input the extension");
-                String extension = scanner.nextLine();
-                System.out.println("Please input directory path");
-                String directoryPath = scanner.nextLine();
-                System.out.println("Please input max quantity of files");
-                int quantityOfFiles = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input the extension");
+        String extension = scanner.nextLine();
+        System.out.println("Please input directory path");
+        String directoryPath = scanner.nextLine();
+        System.out.println("Please input max quantity of files");
+        int quantityOfFiles = scanner.nextInt();
+        System.out.println("Please input quantity of directories");
+        int max = scanner.nextInt();
 
-                File directory = new File(directoryPath);
+        File directory = new File(directoryPath);
 
-                List<File> foundFiles = new ArrayList<>();
-                searchForFiles(directory, extension, foundFiles);
+        List<File> foundFiles = new ArrayList<>();
+        searchForFiles(directory, extension, foundFiles, 0, max);
 
-                if (foundFiles.isEmpty()) {
-                    System.out.println("Files with extension not found");
-                    return;
-                }
-
-                foundFiles.sort(Comparator.comparingLong(File::lastModified).reversed());
-
-                File newestFile = foundFiles.get(0);
-                long newestFileCreationTime = newestFile.lastModified();
-
-                List<File> matchingFiles = new ArrayList<>();
-                matchingFiles.add(newestFile);
-
-                for (int i = 1; i < foundFiles.size(); i++) {
-                    File file = foundFiles.get(i);
-                    long fileCreationTime = file.lastModified();
-                    if (Math.abs(newestFileCreationTime - fileCreationTime) <= 10000 && quantityOfFiles >= matchingFiles.size() ) {
-                        matchingFiles.add(file);
-                    } else {
-                        break;
+        if (foundFiles.isEmpty()) {
+            System.out.println("Files with extension not found");
+            return;
+        }
+        foundFiles.sort(new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                try {
+                    long a = Files.readAttributes(o1.toPath(), BasicFileAttributes.class).creationTime().toMillis();
+                    long b = Files.readAttributes(o2.toPath(), BasicFileAttributes.class).creationTime().toMillis();
+                    if (a > b) {
+                        return -1;
+                    } else if (a == b) {
+                        return 0;
+                    } else if (b > a) {
+                        return 1;
                     }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-
-                System.out.println("List of files with the extension is:");
-                for (File file : matchingFiles) {
-                    System.out.println(file.getName());
-                }
+                return 0;
             }
+        });
 
-            private static void searchForFiles(File directory, String extension, List<File> foundFiles) {
-                File[] files = directory.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        if (file.isFile() && file.getName().endsWith(extension)) {
-                            foundFiles.add(file);
-                        } else if (file.isDirectory()) {
-                            searchForFiles(file, extension, foundFiles);
-                        }
-                    }
+        File newestFile = foundFiles.get(0);
+        long newestFileCreationTime = Files.readAttributes(newestFile.toPath(), BasicFileAttributes.class).creationTime().toMillis();
+
+        List<File> matchingFiles = new ArrayList<>();
+        matchingFiles.add(newestFile);
+
+        List<File> recentFiles = foundFiles.stream().filter(file -> {
+            try {
+                long a = Files.readAttributes(file.toPath(), BasicFileAttributes.class).creationTime().toMillis();
+                long num = newestFileCreationTime - a;
+                return num<10000;
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).toList();
+        if (quantityOfFiles < recentFiles.size()) {
+            System.out.println("log");
+            recentFiles = recentFiles.subList(0, quantityOfFiles);
+        }
+
+        System.out.println("List of files with the extension is:");
+        for (File file : recentFiles) {
+            System.out.println(file.getName());
+        }
+    }
+
+    private static void searchForFiles(File directory, String extension, List<File> foundFiles, int i, int max) {
+        File[] files = directory.listFiles();
+        if (files != null && i <= max) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(extension)) {
+                    foundFiles.add(file);
+
+                } else if (file.isDirectory()) {
+                    searchForFiles(file, extension, foundFiles, i + 1, max);
                 }
             }
         }
+    }
+}
